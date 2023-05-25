194748304ece614980f5a9f346b0f6ae9ae055f1
Futureblur <itsfutureblur@gmail.com>
2023-05-25 13:23:29 +0200

    Initial Commit

---
+++ magnet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  magnet
+//
+//  Created by Rasul Adasov on 25.05.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}

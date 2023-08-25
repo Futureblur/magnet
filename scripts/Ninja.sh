2e26298bb91f0457589bb0cc4b9c6b48e19fccbd
Futureblur <61380971+Futureblur@users.noreply.github.com>
2023-08-25 13:55:55 +0200

    chore(build): update Ninja.sh to include compiler and build type

--- scripts/Ninja.sh
+++ scripts/Ninja.sh
@@ -2,5 +2,5 @@
 
 # This script is used to build the project using the Ninja generator.
 cd ..
-cmake -S . -B magnet/Build -G "Ninja"
+cmake -S . -B magnet/Build -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++
 cmake --build magnet/Build

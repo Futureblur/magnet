b8fd84f1e77d042a0bb52fc2aa69723ed753cb8e
Rasul Adasov <61380971+rdsov@users.noreply.github.com>
2023-08-25 18:01:02 +0200

    Update WindowsInstaller.bat

--- scripts/WindowsInstaller.bat
+++ scripts/WindowsInstaller.bat
@@ -1,25 +1,24 @@
+@echo off
+
 cd ..
 
 echo Generating project files
-
-cmake -S . -B magnet/build -G "Visual Studio 17 2022" -A x64 -DCMAKE_CXX_COMPILER=clang++
+cmake -S . -B magnet/Build -G "Visual Studio 17 2022" -A x64 -DCMAKE_CXX_COMPILER=clang++
 
 echo Building project
-cmake --build magnet/build --config Debug
-
-setlocal
+cmake --build magnet/Build --config Debug
 
 echo Setting up environment variables
 rem Directory containing the magnet executable
-set MAGNET_DIR=%~dp0..\magnet\Binaries\Debug
+set "MAGNET_DIR=%~dp0..\magnet\Binaries\Debug"
 
-rem Add the magnet directory to the PATH
-set PATH=%MAGNET_DIR%;%PATH%
-
-echo Directory added to PATH: %MAGNET_DIR%
-echo New PATH: %PATH%
-
-endlocal
+rem Checking if Magnet is already in the PATH
+echo %PATH% | find /i "%MAGNET_DIR%" > nul
+if errorlevel 1 (
+  rem Add the magnet directory to the PATH
+  setx PATH "%PATH%;%MAGNET_DIR%" /M
+  echo Added magnet to the PATH
+)
 
 echo Launching Visual Studio Solution
 start %~dp0..\magnet\Build\magnet.sln

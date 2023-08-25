37a5aebd49b78393db521bfa555c5c8c42c833a3
Futureblur <61380971+Futureblur@users.noreply.github.com>
2023-08-25 13:49:07 +0200

    build: delete VS22.sh script

--- scripts/VS22.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# This script is used to build the project using the Visual Studio 2022 generator.
-cd ..
-cmake -S . -B magnet/Build -G "Visual Studio 17 2022" -A x64
-cmake --build magnet/Build

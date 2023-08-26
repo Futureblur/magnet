f33b1d1f4b89393aff5942c8ced5ce4e183deee1
Futureblur <61380971+Futureblur@users.noreply.github.com>
2023-08-26 22:13:08 +0200

    chore: rename Unix.sh to Makefile.sh

--- scripts/Makefile.sh
+++ scripts/Makefile.sh
@@ -2,5 +2,5 @@
 
 # This script is used to build the project on Unix systems.
 cd ..
-cmake -S . -B magnet/Build -G "Unix Makefiles"
+cmake -S . -B magnet/Build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
 cmake --build magnet/Build

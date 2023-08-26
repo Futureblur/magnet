d836aa755654e90a69a69e7c5c44313ead002f4a
Futureblur <61380971+Futureblur@users.noreply.github.com>
2023-08-26 15:54:27 +0200

    refactor: remove unused debug code

--- magnet/Source/EntryPoint.cpp
+++ magnet/Source/EntryPoint.cpp
@@ -7,17 +7,7 @@ int main(int argc, const char* argv[])
 	auto args = MG::CommandLineArguments();
 	args.count = argc;
 	args.list = argv;
-
-#ifdef MG_WINDOWS_DEBUG
-	const char* injectedArgs[] = {
-			"magnet",
-			"new"
-	};
-
-	args.count = 2;
-	args.list = injectedArgs;
-#endif
-
+	
 	MG::Application::Init(args);
 	MG::Application::Run();
 

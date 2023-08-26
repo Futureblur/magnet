a9c5754680664a3c70aab0452f8593c168d3ac46
Futureblur <61380971+Futureblur@users.noreply.github.com>
2023-08-26 22:37:27 +0200

    fix(Ninja): use bash instead of ninja and remove compiler type flag

--- scripts/Ninja.sh
+++ scripts/Ninja.sh
@@ -1,6 +1,6 @@
-#!/bin/zsh
+#!/bin/bash
 
 # This script is used to build the project using the Ninja generator.
 cd ..
-cmake -S . -B magnet/Build -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++
+cmake -S . -B magnet/Build -G "Ninja" -DCMAKE_BUILD_TYPE=Debug
 cmake --build magnet/Build

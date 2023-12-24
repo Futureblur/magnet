eebe3514bcc08112042283e474f20130c5523785
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-12-24 13:19:18 +0100

    Fixed UNIX link flags

--- magnet/Source/CommandHandler.cpp
+++ magnet/Source/CommandHandler.cpp
@@ -613,7 +613,7 @@ namespace MG
 		auto ifTrue = [&]()
 		{
 			emitter.Add_Indentation();
-			emitter.Add_SetTargetProperties(projectName, "LINK_FLAGS", "-Wl, -rpath, ./");
+			emitter.Add_SetTargetProperties(projectName, "LINK_FLAGS", "\"-Wl,-rpath,./\"");
 		};
 
 		auto ifFalse = [&]()
